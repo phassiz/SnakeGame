2eb728ba52e99780399e6a445039dfe1fda524bc
Pedro Assiz <phassiz09@gmail.com>
2024-04-03 02:04:20 -0300

    Código para cobra conmer a comida e crescer

--- assets/script/script.js
+++ assets/script/script.js
@@ -65,8 +65,6 @@ function iniciarJogo(){
         snake[0].y = 16 * box;
     }
 
-
-
     criarBG();
     criarCobrinha();
     drawFood();
@@ -79,7 +77,13 @@ function iniciarJogo(){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop(); // Remove o ultimo elemento da cobrinha
+    //snake.pop(); // Remove o ultimo elemento da cobrinha
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x: snakeX,
